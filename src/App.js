4da341575d27acdeed5cb048287bf43eae05a0d2
Darshika <darshika0302@gmail.com>
2022-11-27 20:22:54 +0530

    setting up repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Chart from "./Chart";
+import ylyticlogo from "./assets/ylytic-logo.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>Weekly Subscribers Gain and Loss</h1>
+        <img src={ylyticlogo} width={"150px"} height={"70px"} />
+      </div>
+      <div className="subheading">
+        <h2>
+          Takeaway : Majority of Subscribers are gained in spike (viral) events
+          that mostly occured in Apr-Jun '22
+        </h2>
+      </div>
+
+      <Chart />
     </div>
   );
 }
-
 export default App;
